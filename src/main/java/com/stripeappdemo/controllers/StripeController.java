73b6cd7f06811924c49c05d514b53266a80b203a
Michail <michaildrazdovich@gmail.com>
2017-05-27 00:42:38 +0300

    payment

--- src/main/java/com/stripeappdemo/controllers/StripeController.java
+++ src/main/java/com/stripeappdemo/controllers/StripeController.java
@@ -1,9 +1,11 @@
 package com.stripeappdemo.controllers;
 
+import com.stripe.Stripe;
+import com.stripe.exception.*;
+import com.stripe.model.Charge;
+import com.stripe.model.Customer;
 import com.stripeappdemo.models.CartItem;
 import com.stripeappdemo.models.Product;
-import com.stripeappdemo.models.ShoppingCart;
-import com.stripeappdemo.models.User;
 import com.stripeappdemo.repository.CartItemRepository;
 import com.stripeappdemo.repository.ProductRepository;
 import com.stripeappdemo.repository.ShoppingCartRepository;
@@ -17,7 +19,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/stripe")
@@ -42,7 +46,7 @@ public class StripeController {
 
         BigDecimal total = new BigDecimal(0);
 
-        for(CartItem item : cartItemList){
+        for (CartItem item : cartItemList) {
             total = total.add(item.getSubTotal());
         }
 
@@ -54,7 +58,7 @@ public class StripeController {
     }
 
     @RequestMapping("/addToCart")
-    public String addToCart(@RequestParam("id") Long id){
+    public String addToCart(@RequestParam("id") Long id) {
         Product product = productRepository.findOne(id);
 
         CartItem cartItem = new CartItem();
@@ -67,14 +71,14 @@ public class StripeController {
     }
 
     @RequestMapping("/remove")
-    public String remove(@RequestParam("id") Long id){
+    public String remove(@RequestParam("id") Long id) {
         cartItemRepository.delete(id);
 
         return "redirect:/stripe/";
     }
 
     @RequestMapping(value = "/updateCart", method = RequestMethod.POST)
-    public String updateCart(HttpServletRequest request){
+    public String updateCart(HttpServletRequest request) {
         Long id = Long.parseLong(request.getParameter("id"));
         int qty = Integer.parseInt(request.getParameter("qty"));
 
@@ -85,4 +89,63 @@ public class StripeController {
 
         return "redirect:/stripe/";
     }
+
+    @RequestMapping(value = "/checkoutPay", method = RequestMethod.POST)
+    public String checkoutPay(HttpServletRequest request, Model model) throws CardException, APIException, AuthenticationException, InvalidRequestException, APIConnectionException {
+        Stripe.apiKey = "sk_test_WmBy5orMamktgk8Ole9RBmS4";
+
+// Token is created using Stripe.js or Checkout!
+// Get the payment token submitted by the form:
+        String token = request.getParameter("stripeToken");
+
+// Charge the user's card:
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("amount", 1000);
+        params.put("currency", "usd");
+        params.put("description", "Example charge");
+        params.put("source", token);
+
+        Charge charge = Charge.create(params);
+
+        model.addAttribute("checkoutPaySuccess", true);
+
+        return "forward:/stripe/";
+    }
+
+    @RequestMapping(value = "/elementsPay", method = RequestMethod.POST)
+    public String elementsPay(HttpServletRequest request, Model model) throws CardException, APIException, AuthenticationException, InvalidRequestException, APIConnectionException {
+// Set your secret key: remember to change this to your live secret key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+        Stripe.apiKey = "secret_key";
+
+// Token is created using Stripe.js or Checkout!
+// Get the payment token submitted by the form:
+        String token = request.getParameter("stripeToken");
+
+// Create a Customer:
+        Map<String, Object> customerParams = new HashMap<String, Object>();
+        customerParams.put("email", "testcustomer@example.com");
+        customerParams.put("source", token);
+        Customer customer = Customer.create(customerParams);
+
+// Charge the Customer instead of the card:
+        Map<String, Object> chargeParams = new HashMap<String, Object>();
+        chargeParams.put("amount", 1000);
+        chargeParams.put("currency", "usd");
+        chargeParams.put("customer", customer.getId());
+        Charge charge = Charge.create(chargeParams);
+
+// YOUR CODE: Save the customer ID and other info in a database for later.
+
+// YOUR CODE (LATER): When it's time to charge the customer again, retrieve the customer ID.
+//        Map<String, Object> chargeParams = new HashMap<String, Object>();
+//        chargeParams.put("amount", 1500); // $15.00 this time
+//        chargeParams.put("currency", "usd");
+//        chargeParams.put("customer", customerId);
+//        Charge charge = Charge.create(chargeParams);
+
+        model.addAttribute("elementsPaySuccess", true);
+        return "forward:/stripe/";
+    }
+
 }
